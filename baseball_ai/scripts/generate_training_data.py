9430b304810b053d08296638b3f3cb717527209a
Kyle Posluns <kyleposluns@gmail.com>
2022-04-06 15:32:36 -0400

    created code for simple patterns

--- baseball_ai/scripts/generate_training_data.py
+++ baseball_ai/scripts/generate_training_data.py
@@ -1,9 +1,100 @@
+import argparse
+import json
+import random
 import sys
+from dataclasses import dataclass
+from random import SystemRandom
+from typing import Dict
+
 import jsonstream
+from marshmallow import Schema, fields, post_load, pre_dump
+from rstr import Xeger
+
+parser = argparse.ArgumentParser(description="Create Training Data for baseball-ai")
+parser.add_argument("size", type=int)
+parser.add_argument(
+    "--fixed-input-size", type=int, action="store", required=False, default=None
+)
+
+
+EXCLUDE_ALL = "EXCLUDE_ALL"
+
+
+@dataclass(frozen=True)
+class PatternSpecifier:
+    simple: Dict[str, str]
+    complex: Dict[str, str]
+
+
+@dataclass(frozen=True)
+class TrainingRecipe:
+    alphabet: Dict[str, str]
+    patterns: PatternSpecifier
+
+
+class PatternSpecifierSchema(Schema):
+    simple = fields.Dict(keys=fields.String, values=fields.String)
+    complex = fields.Dict(keys=fields.String, values=fields.String)
+
+    @post_load
+    def __construct(self, data, **kwargs):
+        return PatternSpecifier(**data)
+
+    @pre_dump
+    def __deconstruct(self, data, **kwargs):
+        return data.__dict__
+
+
+class TrainingRecipeSchema(Schema):
+    alphabet = fields.Dict(keys=fields.String(), values=fields.String())
+    patterns = fields.Nested(PatternSpecifierSchema)
+
+    @post_load
+    def __construct(self, data, **kwargs):
+        return TrainingRecipe(**data)
+
+    @pre_dump
+    def __deconstruct(self, data, **kwargs):
+        return data.__dict__
+
 
 def main():
+    args = parser.parse_args()
     json_stream = jsonstream.load(sys.stdin)
 
-    pattern_definition = next(json_stream)
-    print(pattern_definition)
+    recipe = TrainingRecipeSchema().load(next(json_stream))
+    simple_values = list(recipe.patterns.simple.items())
+    valid_patterns = list(
+        filter(lambda x: x != EXCLUDE_ALL, list(recipe.patterns.simple.values()))
+    )
 
+    complex_values = list(recipe.patterns.complex.items())
+
+    xeger = Xeger(SystemRandom())
+
+    training_data = {}
+    alphabet = list(recipe.alphabet.keys())
+    while len(training_data) < args.size:
+        if len(recipe.patterns.complex) > 0 and random.random() < 0.5:
+            choice = random.choice(complex_values)
+            training_data[xeger.xeger(choice[1])] = choice[0].upper()
+        else:
+            choice = random.choice(simple_values)
+            include = choice[1]
+            include_list = [include] if include != EXCLUDE_ALL else []
+            exclude_list = [] if include != EXCLUDE_ALL else valid_patterns
+            if args.fixed_input_size:
+                data = xeger.rstr(
+                        alphabet,
+                        args.fixed_input_size,
+                        include=include_list
+                    )
+            else:
+                data = xeger.rstr(alphabet, include=include_list)
+
+            if any([exclude in data for exclude in exclude_list]):
+                continue
+            else:
+                training_data[data] = choice[0].upper()
+
+    print(json.dumps(training_data))
--- setup.py
+++ setup.py
@@ -3,14 +3,8 @@ from setuptools import setup
 setup(
     name="baseball-ai",
     version="1.0",
-    packages=[
-        "baseball_ai",
-        "baseball_ai.scripts"
-    ],
-    install_requires=[
-        "marshmallow==3.13.0",
-        "jsonstream==0.0.1",
-    ],
+    packages=["baseball_ai", "baseball_ai.scripts"],
+    install_requires=["marshmallow==3.13.0", "jsonstream==0.0.1", "rstr==3.1.0"],
     author="kyle",
     entry_points={
         "console_scripts": [
