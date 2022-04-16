2498a5caac7bed582d89c0b40cddec011eae4659
Kyle Posluns <kyleposluns@gmail.com>
2022-04-16 13:57:11 -0400

    got accuracy to 100

--- baseball_ai/find_simple_pattern.py
+++ baseball_ai/find_simple_pattern.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser(description="Lets train a model")
 parser.add_argument(
     "training_data_file", action="store", type=lambda p: Path(p).absolute()
 )
+parser.add_argument("testing_data_file", action="store", type=lambda p: Path(p).absolute())
 
 
 def intersection(lst1, lst2):
@@ -28,7 +29,7 @@ def find_patterns(training_inputs, training_outputs):
                 output_to_pattern.get(output), list(input)
             )
 
-    return {key: "".join(value) for key, value in output_to_pattern.items()}
+    return {"".join(value): key for key, value in output_to_pattern.items()}
 
 
 def read_data_file(training_file):
@@ -37,10 +38,37 @@ def read_data_file(training_file):
 
     return list(training_data.keys()), list(training_data.values())
 
+def test(patterns, test_inputs, test_outputs):
+    amt_correct = 0
+    amt_seen = 0
+    for i in range(len(test_inputs)):
+        input = test_inputs[i]
+        output = test_outputs[i]
+
+        chosen_output = "NONE"
+        for pattern, result in patterns.items():
+            if pattern != "" and pattern in input:
+                chosen_output = result
+                break
+
+
+        if output == chosen_output:
+            amt_correct += 1
+
+        amt_seen += 1
+    accuracy = amt_correct / amt_seen
+    print(f"Accuracy: {accuracy * 100}%")
+
+
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
 
     training_inputs, training_outputs = read_data_file(args.training_data_file)
+    test_inputs, test_outputs = read_data_file(args.testing_data_file)
 
-    print(find_patterns(training_inputs, training_outputs))
+    patterns = find_patterns(training_inputs, training_outputs)
+
+    test(patterns, test_inputs, test_outputs)
+
